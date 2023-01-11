c19548111b297bcd741088cb5537f63170ee6d09
dagume <guechadaniel@gmail.com>
2023-01-11 12:02:36 -0500

    🚧 working to login

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,8 +5,10 @@ import { AuthService } from './services/auth.service';
 import { LocalStrategy } from './strategies/local.strategy';
 
 import { UsersModule } from '../users/users.module';
+import { AuthController } from './controllers/auth.controller';
 @Module({
   imports: [UsersModule, PassportModule],
   providers: [AuthService, LocalStrategy],
+  controllers: [AuthController],
 })
 export class AuthModule {}
